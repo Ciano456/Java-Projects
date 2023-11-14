9e4f87b93ecb52b14ff5ca5d98e4f933e1819465
Cian O'Connor <cianoconnor713@gmail.com>
2023-11-14 12:58:32 +0000

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Carpet carpet = new Carpet(3.5);
+        Floor floor = new Floor(2.75, 4.0);
+
+        Calculator calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+
+        carpet = new Carpet(1.5);
+        floor = new Floor(5.4, 4.5);
+
+        calculator = new Calculator(floor, carpet);
+        System.out.println("total= " + calculator.getTotalCost());
+    }
+}
